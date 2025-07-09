0da9798e5f4868d8630c5f67ec73592a70afe37d
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-07-09 21:31:30 +0700

    Migrate to setprop

--- Modules/Kamui/Bastion.sh
+++ Modules/Kamui/Bastion.sh
@@ -1,71 +1,71 @@
 #!/bin/bash
 
 # Battery Modifications
-resetprop -n persist.sys.shutdown.mode hibernate
-resetprop -n persist.radio.add_power_save 1
-resetprop -n wifi.supplicant_scan_interval 300 
-resetprop -n ro.ril.disable.power.collapse 1
-resetprop -n ro.config.hw_fast_dormancy 1
-resetprop -n ro.semc.enable.fast_dormancy true
-resetprop -n ro.config.hw_quickpoweron true
-resetprop -n ro.mot.eri.losalert.delay 1000
-resetprop -n ro.config.hw_power_saving true
-resetprop -n pm.sleep_mode 1
-resetprop -n ro.ril.sensor.sleep.control 1
-resetprop -n power_supply.wakeup enable
+setprop persist.sys.shutdown.mode hibernate
+setprop persist.radio.add_power_save 1
+setprop wifi.supplicant_scan_interval 300 
+setprop ro.ril.disable.power.collapse 1
+setprop ro.config.hw_fast_dormancy 1
+setprop ro.semc.enable.fast_dormancy true
+setprop ro.config.hw_quickpoweron true
+setprop ro.mot.eri.losalert.delay 1000
+setprop ro.config.hw_power_saving true
+setprop pm.sleep_mode 1
+setprop ro.ril.sensor.sleep.control 1
+setprop power_supply.wakeup enable
 
 # Additional Battery Optimizations
-resetprop -n ro.ril.power.collapse 1
-resetprop -n power.saving.enabled 1
-resetprop -n battery.saver.low_level 30
-resetprop -n power.saving.enable 1
-resetprop -n persist.radio.apm_sim_not_pwdn 1
-resetprop -n ro.ril.enable.amr.wideband 0
-resetprop -n power.saving.low_screen_brightness 1
-resetprop -n ro.config.hw_smart_battery 1
-resetprop -n ro.config.hw_power_profile low
+setprop ro.ril.power.collapse 1
+setprop power.saving.enabled 1
+setprop battery.saver.low_level 30
+setprop power.saving.enable 1
+setprop persist.radio.apm_sim_not_pwdn 1
+setprop ro.ril.enable.amr.wideband 0
+setprop power.saving.low_screen_brightness 1
+setprop ro.config.hw_smart_battery 1
+setprop ro.config.hw_power_profile low
 
 # Dalvik and Kernel Modifications
-resetprop -n persist.android.strictmode 0
-resetprop -n ro.kernel.android.checkjni 0
-resetprop -n ro.kernel.checkjni 0
-resetprop -n ro.config.nocheckin 1
-resetprop -n ro.compcache.default 0
-resetprop -n dalvik.vm.execution-mode int:jit
-resetprop -n dalvik.vm.verify-bytecode true
-resetprop -n dalvik.vm.jmiopts forcecopy
-resetprop -n debug.kill_allocating_task 0
-resetprop -n ro.ext4fs 1
-resetprop -n ro.setupwizard.mode DISABLED
-resetprop -n dalvik.vm.heaputilization 0.25
-resetprop -n dalvik.vm.heaptargetutilization 0.75
+setprop persist.android.strictmode 0
+setprop ro.kernel.android.checkjni 0
+setprop ro.kernel.checkjni 0
+setprop ro.config.nocheckin 1
+setprop ro.compcache.default 0
+setprop dalvik.vm.execution-mode int:jit
+setprop dalvik.vm.verify-bytecode true
+setprop dalvik.vm.jmiopts forcecopy
+setprop debug.kill_allocating_task 0
+setprop ro.ext4fs 1
+setprop ro.setupwizard.mode DISABLED
+setprop dalvik.vm.heaputilization 0.25
+setprop dalvik.vm.heaptargetutilization 0.75
 
 # Disable USB Debugging Popup
-resetprop -n persist.adb.notify 0
+setprop persist.adb.notify 0
 
 # Allow to free more RAM
-resetprop -n persist.sys.purgeable_assets 1
-resetprop -n ro.config.low_ram enable
+setprop persist.sys.purgeable_assets 1
+setprop ro.config.low_ram enable
 
 # Smoother video playback
-resetprop -n video.accelerate.hw 1
-resetprop -n media.stagefright.enable-player true
-resetprop -n media.stagefright.enable-meta true
-resetprop -n media.stagefright.enable-scan false
-resetprop -n media.stagefright.enable-http true
+setprop video.accelerate.hw 1
+setprop media.stagefright.enable-player true
+setprop media.stagefright.enable-meta true
+setprop media.stagefright.enable-scan false
+setprop media.stagefright.enable-http true
 
 # UI Tweaks
-resetprop -n persist.sys.ui.hw 1
-resetprop -n view.scroll_friction 10
-resetprop -n debug.composition.type gpu
-resetprop -n debug.performance.tuning 1
+setprop persist.sys.ui.hw 1
+setprop view.scroll_friction 10
+setprop debug.composition.type gpu
+setprop debug.performance.tuning 1
 
 # Miscellaneous
-resetprop -n persist.sys.gmaps_hack 1
-resetprop -n debug.sf.ddms 0
-resetprop -n ro.warmboot.capability 1
-resetprop -n logcat.live disable
+setprop persist.sys.gmaps_hack 1
+setprop debug.sf.ddms 0
+setprop ro.warmboot.capability 1
+setprop logcat.live disable
 
 # CPU Core Control
-resetprop -n ro.vendor.qti.core_ctl_min_cpu 4
-resetprop -n ro.vendor.qti.core_ctl_max_cpu 4
+setprop ro.vendor.qti.core_ctl_min_cpu 4
+setprop ro.vendor.qti.core_ctl_max_cpu 4
