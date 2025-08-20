d8a4e3f27128174bb852bf1216b25430311f624d
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-08-20 07:43:45 +0700

    CPU Freq tweak & Power save on / off

--- Modules/Kamui/KamuiBalanced.sh
+++ Modules/Kamui/KamuiBalanced.sh
@@ -6,6 +6,10 @@ tweak() {
     fi
 }
 
+#############################
+# Set Governor To Schedhorizon / Schedutil
+#############################
+
 check_governor() {
     local governor="$1"
     local available_governors="$2"
@@ -23,3 +27,29 @@ for path in /sys/devices/system/cpu/cpufreq/policy*; do
         fi
     fi
 done
+
+#############################
+# Set CPU Freq to Normal
+#############################
+
+cluster=0
+	for path in /sys/devices/system/cpu/cpufreq/policy*; do
+		cpu_maxfreq=$(<"$path/cpuinfo_max_freq")
+		cpu_minfreq=$(<"$path/cpuinfo_min_freq")
+		tweak "$cluster $cpu_maxfreq" /proc/ppm/policy/hard_userlimit_max_cpu_freq
+		tweak "$cluster $cpu_minfreq" /proc/ppm/policy/hard_userlimit_min_cpu_freq
+		((cluster++))
+	done
+
+for path in /sys/devices/system/cpu/*/cpufreq; do
+		cpu_maxfreq=$(<"$path/cpuinfo_max_freq")
+		cpu_minfreq=$(<"$path/cpuinfo_min_freq")
+		tweak "$cpu_maxfreq" "$path/scaling_max_freq"
+		tweak "$cpu_minfreq" "$path/scaling_min_freq"
+	done
+	chmod -f 644 /sys/devices/system/cpu/cpufreq/policy*/scaling_*_freq
+
+#############################
+# Power Save Mode Off
+#############################
+settings put global low_power 0
--- Modules/Kamui/KamuiPowersave.sh
+++ Modules/Kamui/KamuiPowersave.sh
@@ -6,7 +6,35 @@ tweak() {
     fi
 }
 
+#############################
+# Set Governor To Minimum
+#############################
+
 # Switch to powersave
 for path in /sys/devices/system/cpu/cpufreq/policy*; do
 	tweak powersave $path/scaling_governor
 done 
+
+#############################
+# Set CPU Freq to Min
+#############################
+
+cluster=-1
+	for path in /sys/devices/system/cpu/cpufreq/policy*; do
+		((cluster++))
+		cpu_minfreq=$(<"$path/cpuinfo_min_freq")
+		tweak "$cluster $cpu_minfreq" /proc/ppm/policy/hard_userlimit_max_cpu_freq
+		tweak "$cluster $cpu_minfreq" /proc/ppm/policy/hard_userlimit_min_cpu_freq
+	done
+
+for path in /sys/devices/system/cpu/*/cpufreq; do
+		cpu_minfreq=$(<"$path/cpuinfo_min_freq")
+		tweak "$cpu_minfreq" "$path/scaling_max_freq"
+		tweak "$cpu_minfreq" "$path/scaling_min_freq"
+	done
+	chmod -f 444 /sys/devices/system/cpu/cpufreq/policy*/scaling_*_freq
+
+#############################
+# Power Save Mode On
+#############################
+settings put global low_power 1
